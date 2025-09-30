29cfb99d35f4e0786341a3972fb2bcfea48652b4
Soares <rossoares@deloitte.com>
2025-09-29 21:06:22 -0300

    Super_trufo.c

---
+++ Desafio Super Trunfo - Países - Cadastro das Cartas/Desafio_Mestre.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    // Carta 1
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50];
+    unsigned long int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+    float densidade1, pibPerCapita1, superPoder1;
+
+    // Carta 2
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[50];
+    unsigned long int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+    float densidade2, pibPerCapita2, superPoder2;
+
+    // Entrada - Carta 1
+    printf("Cadastro da Carta 1:\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade1);
